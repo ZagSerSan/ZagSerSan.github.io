cd7ee791a824e52612dd9e3732cbfabc7971bc40
ZagSerSan <zagsersan2017@tutamail.com>
2019-12-26 12:52:07 +0200

    init commit

--- works/mogo/app.js
+++ works/mogo/app.js
@@ -29,4 +29,20 @@ $(function() {
 	});
 
 
+	// clouse of scroll and tap
+	$(document).mouseup(function (e){ // отслеживаем событие клика по веб-документу
+        let btn = $("#burger"); // определяем элемент, к которому будем применять условия 
+        						//(можем указывать ID, класс либо любой другой идентификатор элемента)
+        if (!btn.is(e.target) // проверка условия если клик был не по нашему блоку
+            && btn.has(e.target).length === 0) { // проверка условия если клик не по его дочерним элементам
+            btn.removeClass("active"); // если условия выполняются - скрываем наш элемент
+        }
+    });
+
+	$(document).scroll(function (e){
+        if (!btn.is(e.target) && btn.has(e.target).length === 0) {
+            btn.removeClass("active");
+        }
+    });
+
 });

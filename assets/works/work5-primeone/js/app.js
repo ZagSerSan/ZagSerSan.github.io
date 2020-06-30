03cc7cf5359ca8baf4663b601c00b61d192f9474
ZagSerSan <zagsersan2017@tutamail.com>
2020-06-30 17:05:21 +0300

    done header

--- assets/works/work5-primeone/js/app.js
+++ assets/works/work5-primeone/js/app.js
@@ -1,1 +1,28 @@
-$((function(){let e=$("#header-burger");e.on("click",(function(t){t.preventDefault(),$(e).toggleClass("active")}))}));
+$(function () {
+
+//=====================burger==========================
+
+	let header_burger = $("#header-burger");
+	let header_burger_icon = $("#header-burger-icon");
+
+	header_burger_icon.on('click', function (event) {
+		event.preventDefault();
+
+		header_burger.toggleClass('active');
+
+	});
+
+	let data_closeburger = $("[data-closeburger]");
+
+	data_closeburger.on('click', function (event) {
+		event.preventDefault();
+
+		let $this = $(this);
+		let burger = $this.data('closeburger');
+
+		$(burger).removeClass('active');
+
+	});
+
+
+});

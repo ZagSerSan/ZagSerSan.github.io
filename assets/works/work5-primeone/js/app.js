c841f95470a32dbb68c832c87ac53483a9fdec8a
ZagSerSan <zagsersan2017@tutamail.com>
2020-07-02 21:23:21 +0300

    +'intro', and some "product"

--- assets/works/work5-primeone/js/app.js
+++ assets/works/work5-primeone/js/app.js
@@ -25,18 +25,36 @@ $(function () {
 	});
 
 
-//=====================Script==========================
+//=====================Script bgimgjs==========================
 
-	// получить атрибут 'src';
-	let img_src = document.getElementById('intro-img').src;
+  //============intro//============
+	let intro_isrc = document.getElementById('intro-bgimgjs').src;
+	
+	$('.intro-bgimgjs img').css({'display':'none'});
+	
+	$('.intro-bgimgjs').css({'background-image':'url('+intro_isrc+')',
+							'background-position':'center',
+							'background-repeat':'no-repeat',
+							'background-size':'cover'});
+	
+  //============products//============
+	let products_isrc1 = document.getElementById('products-bgimgjs-1').src;
+	let products_isrc2 = document.getElementById('products-bgimgjs-2').src;
+	$('.products-bgimgjs-1 img').css({'opacity':'0',
+							'width':'100%',
+							'height':'auto'});
+	$('.products-bgimgjs-2 img').css({'opacity':'0',
+							'width':'100%',
+							'height':'auto'});
 
-	//скрыть img в html
-	$('.intro-column img').css({
-	  'display':'none'
-	});
-
-	//img_src засунуть в bg
-	$('.bgimg').css('background-image' ,'url('+img_src+')');
+	$('.products-bgimgjs-1').css({'background-image':'url('+products_isrc1+')',
+							'background-position':'center',
+							'background-repeat':'no-repeat',
+							'background-size':'cover'});
+	$('.products-bgimgjs-2').css({'background-image':'url('+products_isrc2+')',
+							'background-position':'center',
+							'background-repeat':'no-repeat',
+							'background-size':'cover'});
 
 
 });

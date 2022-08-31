9b881319050bb78e045994145d4011bde245f693
ZagSerSan <yan.steys1997@gmail.com>
2022-08-31 11:51:18 +0200

    fixed header

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(function() {
+
+   let header = $("#header");
+   let intro = $("#intro");
+   let introHeight;
+   let scrollPos = $(window).scrollTop();
+
+   $(window).on("scroll load resize", function() {
+
+      introHeight = intro.innerHeight()-50;
+      scrollPos = $(this).scrollTop();
+
+      if (scrollPos > introHeight) {
+         header.addClass('fixed');
+      } else {
+         header.removeClass('fixed');
+      }
+
+      console.log("introHeight = "+introHeight);
+      console.log("scrollPos = "+scrollPos);
+      
+   });
+   
+
+});

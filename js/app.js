564b587e537de91334207e2d494710fb18dbc02a
ZagSerSan <yan.steys1997@gmail.com>
2022-09-01 11:08:52 +0200

    js: fixed header and smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,46 +1,39 @@
+$(function() {
+   
+// fixed header
+   let header = $("#header");
+   let intro = $("#intro");
+   let introHeight = intro.innerHeight()-50;
+   let scrollPos = $(window).scrollTop();
 
+   checkScroll(scrollPos, introHeight)
 
+   $(window).on("scroll load resize", function() {
 
+      introHeight = intro.innerHeight()-50;
+      scrollPos = $(window).scrollTop();
 
+      checkScroll(scrollPos, introHeight)
+   });
 
+   function checkScroll(scrollPos, introHeight) {
+      if(scrollPos > introHeight) {
+         header.addClass('fixed');
+      } else {
+         header.removeClass('fixed');
+      }
+   }
 
+   // smooth skroll
+   $("[data-scroll]").on("click", function(even) {
+      even.preventDefault();
 
+      let elementId = $(this).data('scroll'); // get id elements in variable
+      let elementOffset = $(elementId).offset().top;
 
+      $('html, body').animate({
+         scrollTop: elementOffset - 90
+      }, 700);
+   });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(function() {
-
-//    let header = $("#header");
-//    let intro = $("#intro");
-//    let introHeight;
-//    let scrollPos = $(window).scrollTop();
-
-//    $(window).on("scroll load resize", function() {
-
-//       introHeight = intro.innerHeight()-50;
-//       scrollPos = $(this).scrollTop();
-
-//       if (scrollPos > introHeight) {
-//          header.addClass('fixed');
-//       } else {
-//          header.removeClass('fixed');
-//       }
-
-//       console.log("introHeight = "+introHeight);
-//       console.log("scrollPos = "+scrollPos);
-      
-//    });
-
-// });
+});

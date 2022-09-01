64fb5d71bf5924f9f77d103081ebbb532156a9f9
ZagSerSan <yan.steys1997@gmail.com>
2022-09-01 10:25:13 +0200

    try again js part (fixed header)

--- js/app.js
+++ js/app.js
@@ -1,25 +1,46 @@
-$(function() {
 
-   let header = $("#header");
-   let intro = $("#intro");
-   let introHeight;
-   let scrollPos = $(window).scrollTop();
 
-   $(window).on("scroll load resize", function() {
 
-      introHeight = intro.innerHeight()-50;
-      scrollPos = $(this).scrollTop();
 
-      if (scrollPos > introHeight) {
-         header.addClass('fixed');
-      } else {
-         header.removeClass('fixed');
-      }
 
-      console.log("introHeight = "+introHeight);
-      console.log("scrollPos = "+scrollPos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+
+//    let header = $("#header");
+//    let intro = $("#intro");
+//    let introHeight;
+//    let scrollPos = $(window).scrollTop();
+
+//    $(window).on("scroll load resize", function() {
+
+//       introHeight = intro.innerHeight()-50;
+//       scrollPos = $(this).scrollTop();
+
+//       if (scrollPos > introHeight) {
+//          header.addClass('fixed');
+//       } else {
+//          header.removeClass('fixed');
+//       }
+
+//       console.log("introHeight = "+introHeight);
+//       console.log("scrollPos = "+scrollPos);
       
-   });
-   
+//    });
 
-});
+// });

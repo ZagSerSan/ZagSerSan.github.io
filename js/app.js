33d0f672a556a9ab5aa18580e4d1a4966fd6bbe6
ZagSerSan <yan.steys1997@gmail.com>
2022-09-01 11:45:53 +0200

    scroll and show/hide menu

--- js/app.js
+++ js/app.js
@@ -5,15 +5,16 @@ $(function() {
    let intro = $("#intro");
    let introHeight = intro.innerHeight()-50;
    let scrollPos = $(window).scrollTop();
+   let nav = $('#nav')
+   let navToggle = $('#navToggle');
 
    checkScroll(scrollPos, introHeight)
 
    $(window).on("scroll load resize", function() {
-
       introHeight = intro.innerHeight()-50;
       scrollPos = $(window).scrollTop();
-
       checkScroll(scrollPos, introHeight)
+      nav.removeClass('active');
    });
 
    function checkScroll(scrollPos, introHeight) {
@@ -27,13 +28,17 @@ $(function() {
    // smooth skroll
    $("[data-scroll]").on("click", function(even) {
       even.preventDefault();
-
       let elementId = $(this).data('scroll'); // get id elements in variable
       let elementOffset = $(elementId).offset().top;
-
       $('html, body').animate({
          scrollTop: elementOffset - 90
       }, 700);
+   });
+
+   // nav toggle
+   navToggle.on('click', function(even) {
+      even.preventDefault();
+      nav.toggleClass('active')
    });
 
 });

76acae5252de3cd1383ddb6e5ec8ae69b9819dda
ZagSerSan <yan.steys1997@gmail.com>
2022-09-02 09:59:48 +0200

    + smoth scroll and active burger

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ $(function() {
    let intro = $('#intro');
    let introHeight = intro.innerHeight()-50;
    let scrollPosition = $(window).scrollTop();
+   let nav = $('#nav');
+   let burger = $('#burger')
 
    checkScroll (scrollPosition, introHeight);
    
@@ -13,6 +15,7 @@ $(function() {
       introHeight = intro.innerHeight()-50;
       scrollPosition = $(window).scrollTop();
       checkScroll (scrollPosition, introHeight);
+      nav.removeClass('active');
    });
 
    function checkScroll (scrollPosition, introHeight) {
@@ -23,42 +26,8 @@ $(function() {
       }
    }
 
-
-
-
-});
-
-
-
-   /*
-   $(function() {
-// fixed header
-   let header = $("#header");
-   let intro = $("#intro");
-   let introHeight = intro.innerHeight()-50;
-   let scrollPos = $().scrollTop();
-   let nav = $('#nav')
-   let navToggle = $('#navToggle');
-
-   checkScroll(scrollPos, introHeight)
-
-   $(window).on("scroll load resize", function() {
-      introHeight = intro.innerHeight()-50;
-      scrollPos = $(window).scrollTop();
-      checkScroll(scrollPos, introHeight)
-      nav.removeClass('active');
-   });
-
-   function checkScroll(scrollPos, introHeight) {
-      if(scrollPos > introHeight) {
-         header.addClass('fixed');
-      } else {
-         header.removeClass('fixed');
-      }
-   }
-
-   // smooth skroll
-   $("[data-scroll]").on("click", function(even) {
+   // smooth scroll by navigation links
+   $('[data-scroll]').on("click", function(even) {
       even.preventDefault();
       let elementId = $(this).data('scroll'); // get id elements in variable
       let elementOffset = $(elementId).offset().top;
@@ -68,10 +37,9 @@ $(function() {
    });
 
    // nav toggle
-   navToggle.on('click', function(even) {
+   burger.on('click', function (even) {
       even.preventDefault();
-      nav.toggleClass('active')
+      nav.toggleClass('active');
    });
+   
 });
-   */
-

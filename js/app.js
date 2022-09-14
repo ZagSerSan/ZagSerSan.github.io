e46d40879a830ca8e196d43c5ff8e090113631ef
ZagSerSan <yan.steys1997@gmail.com>
2022-09-14 08:02:47 +0200

    call menu by ckick burger button and scroll to

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(function() {
+
+//todo получить доступ с бургеру
+
+let burger = $('#burger');
+let nav = $('#header-nav');
+let header = $("#header");
+let introH = $('#intro').innerHeight()-50;;
+let scrollPos = $(window).scrollTop();
+
+// call menu by ckick burger button
+burger.on('click', function(even) {
+   even.preventDefault();
+   nav.toggleClass('active');
+});
+
+// fixed header
+$(window).on('scroll load resize', function() {
+   introH = $('#intro').innerHeight()-50;
+   scrollPos = $(window).scrollTop();
+   nav.removeClass('active');
+   if (scrollPos > introH) {
+      header.addClass('fixed');
+   } else {
+      header.removeClass('fixed');
+   }
+});
+
+// скролл до блока
+
+
+});

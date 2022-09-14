01419fa8c2001c5fdea107f778469947cf6f21a6
ZagSerSan <yan.steys1997@gmail.com>
2022-09-14 08:59:56 +0200

    learning data-* attribute

--- js/app.js
+++ js/app.js
@@ -6,25 +6,39 @@ let header = $("#header");
 let introH = $('#intro').innerHeight()-50;;
 let scrollPos = $(window).scrollTop();
 
+checkScroll(scrollPos, introH);
+
+// fixed header
+$(window).on('scroll load resize', function(even) {
+   even.preventDefault();
+   introH = $('#intro').innerHeight()-50;
+   scrollPos = $(window).scrollTop();
+   checkScroll(scrollPos, introH);
+   nav.removeClass('active');
+});
+function checkScroll(scrollPos, introH) {
+   if (scrollPos > introH) {
+      header.addClass('fixed');
+   } else {
+      header.removeClass('fixed');
+   }
+}
+
 // call menu by ckick burger button
 burger.on('click', function(even) {
    even.preventDefault();
    nav.toggleClass('active');
 });
 
-// fixed header
-$(window).on('scroll load resize', function() {
-   introH = $('#intro').innerHeight()-50;
-   scrollPos = $(window).scrollTop();
-   nav.removeClass('active');
-   if (scrollPos > introH) {
-      header.addClass('fixed');
-   } else {
-      header.removeClass('fixed');
-   }
-});
+// smooth scroll by navigation links
 
-// скролл до блока
 
 
 });
+
+
+var article = document.getElementById('header-nav__link');
+var test = article.getAttribute('data-scroll');
+
+console.log(test); // #features
+

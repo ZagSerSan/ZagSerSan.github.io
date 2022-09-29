9f73edc035890724164b24dcf3d7f8a64a5ccdd0
ZagSerSan <yan.steys1997@gmail.com>
2022-09-29 17:55:15 +0200

    block 'about' is done, corrected js in menu button

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ $(function () {
    
    $('[data-link]').on('click', function (even) {
      even.preventDefault();
+     
+     var linkJs = $("a[data-link]");
+     linkJs.removeClass('active');
      var $this = $(this);
      $this.toggleClass('active');
    });
-   
    
    
    });

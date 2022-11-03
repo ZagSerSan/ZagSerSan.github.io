2017108f6c8db16970929664d022ba8427f54d14
ZagSerSan <yan.steys1997@gmail.com>
2022-11-03 10:46:01 +0100

    forEach (my code)

--- js/app2.js
+++ js/app2.js
@@ -2,22 +2,33 @@ var header = document.getElementById('headerJS');
 var introH;
 var scrollPos;
 
-var navLink = document.getElementById('navLinkJs');
-
 window.addEventListener('scroll', function (scrollPos,introH,navLink) {
    introH = document.getElementById('introJS').scrollHeight - 70;
    scrollPos = window.pageYOffset;
 
-   const navLinks = document.querySelectorAll(navLink);
-
    if (scrollPos > introH) {
       header.classList.add('fixed');
-   console.log(navLinks);
    } else {
       header.classList.remove('fixed');
    }
 
    console.log(introH);
    console.log(scrollPos);
-})
+});
 
+
+function activeLink(elSelector) {
+   const navLinks = document.querySelectorAll(elSelector);
+
+   navLinks.forEach((thisLink) => {
+      thisLink.addEventListener('click', (e) => {
+         navLinks.forEach((allLinks) => {
+            allLinks.classList.remove('active');
+         });
+         e.preventDefault();
+         thisLink.classList.add('active');
+      });
+   });
+}
+
+activeLink('#navLinkJs');
